c927723587325b0c9d99dd29d2929dcdb3ba7cbb
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-03-23 12:39:09 +0100

    chore: Update packages and remove unneeded files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,3 @@
-const path = require('path')
-const toPath = _path => path.join(process.cwd(), _path)
-
 module.exports = {
-  stories: ['../src/*.stories.js'],
-  webpackFinal: async config => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          // Fix for storybook saying @emotion/styled is actually emotion-styled.
-          '@emotion/styled': toPath('node_modules/@emotion/styled'),
-        },
-      },
-    }
-  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 }
