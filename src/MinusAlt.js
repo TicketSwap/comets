36b1d9371a37a362e482891951b337d34eea4f73
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-03-23 23:41:09 +0100

    feat: make sure to use typescript for each component

--- src/MinusAlt.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Icon } from './Icon'
-
-export const MinusAlt = props => (
-  <Icon a11yTitle="MinusAlt" {...props}>
-    <path d="M7 15a1 1 0 1 0 0 2h18a1 1 0 1 0 0-2H7z" />
-  </Icon>
-)
--- src/Row.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Icon } from './Icon'
-
-export const Row = props => (
-  <Icon a11yTitle="Row" {...props}>
-    <path d="M24 10H8a2 2 0 0 1 0-4h16a2 2 0 0 1 0 4zm3 9H5a3 3 0 1 1 0-6h22a3 3 0 1 1 0 6zm-3 7H8a2 2 0 0 1 0-4h16a2 2 0 0 1 0 4z" />
-  </Icon>
-)
