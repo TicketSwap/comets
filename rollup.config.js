29e7713223adb45ad236e92bae09a29a75cc6e2e
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-03-23 12:52:18 +0100

    chore: replace uglify with terser
    Terser has better minify results than uglify.

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,22 @@
 import { babel } from '@rollup/plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import { uglify } from 'rollup-plugin-uglify'
 import replace from '@rollup/plugin-replace'
 import visualizer from 'rollup-plugin-visualizer'
+import filesize from 'rollup-plugin-filesize'
 import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
       file: pkg.module,
       format: 'esm',
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
     },
   ],
   plugins: [
@@ -32,7 +33,10 @@ export default {
       babelHelpers: 'bundled',
       presets: ['@babel/preset-env', '@babel/preset-react'],
     }),
-    uglify(),
+    terser(),
     visualizer(),
+    filesize(),
   ],
+  external: id =>
+    !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/'),
 }
